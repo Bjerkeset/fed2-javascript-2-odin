32501ab61cc0ac324b7648080bab22e4d44cb9cf
KrystianGH2 <112473660+KrystianGH2@users.noreply.github.com>
2023-10-10 11:00:52 +0200

    Login form - torjus

--- app/(site)/login/page.tsx
+++ app/(site)/login/page.tsx
@@ -1,19 +1,35 @@
+import LoginForm from "@/components/LoginForm";
+
 import AuthButtonClient from "@/components/shared/buttons/AuthButtonClient";
-import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
-import {cookies} from "next/headers";
-import {redirect} from "next/navigation";
+
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+
+import { cookies } from "next/headers";
+
+import { redirect } from "next/navigation";
+
 import React from "react";
 
 export default async function page() {
-  const supabase = createServerComponentClient({cookies});
-  const {data: session} = await supabase.auth.getSession();
+  const supabase = createServerComponentClient({ cookies });
 
-  if (session) {
-    redirect("/test");
+  const { data: session } = await supabase.auth.getSession();
+
+  {
+    /* {
+
+    if (session) {
+
+      redirect("/test");
+
+    } */
   }
+
   return (
     <div>
-      <AuthButtonClient session={session} />
+      <LoginForm />
+
+      {/* <AuthButtonClient session={session}  /> */}
     </div>
   );
 }
