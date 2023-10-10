5a688cb7085a6fd4f1ad0632cf9d801a1e2f44be
Bjerkeset <bb.bjerk@gmail.com>
2023-10-10 19:00:57 +0200

    Fetch function on homepage.

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Feed from "@/components/pages/home/Feed";
+import NewPostForm from "@/components/pages/home/NewPostForm";
 import AuthButtonServer from "@/components/shared/buttons/AuthButtonServer";
 import ProfileList from "@/components/shared/cards/ProfileList";
 import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
@@ -13,11 +14,23 @@ export default async function Home() {
   //   redirect("/login");
   // }
 
-  const {data: posts} = await supabase.from("posts").select();
+  const {data: posts} = await supabase.from("posts").select("*, profiles(*)");
+
+  fetch("/api/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      post: "hello world",
+    }),
+  });	
+
+  fetch("/api/profules", {
 
   return (
     <section className="flex flex-col items-center max-w-3xl">
-      <h1 className="text-4xl">Homepage</h1>
+      <NewPostForm />
       <div>
         <AuthButtonServer />
       </div>
---
+++ components/pages/home/NewPostForm.tsx
@@ -0,0 +1,66 @@
+"use client";
+import * as z from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import {Button} from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
+import {Card, CardContent} from "@/components/ui/card";
+import {Textarea} from "@/components/ui/textarea";
+import {onSubmit} from "@/constants/submitHandler";
+
+const formSchema = z.object({
+  post: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export default function NewPostForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      post: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    "use server";
+    console.log(values);
+    const supabase = createServerActionClient({cookies});
+  }
+  return (
+    <Card className="w-full flex">
+      <CardContent className="w-full">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="post"
+              render={({field}) => (
+                <FormItem>
+                  <FormLabel>Post</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>This is your post</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="center">
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
