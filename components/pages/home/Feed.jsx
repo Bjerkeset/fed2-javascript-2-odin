c76e3481dfaa83896ff4a56594d9b5ea2dd9c33c
KrystianGH2 <112473660+KrystianGH2@users.noreply.github.com>
2023-10-12 21:25:02 +0200

    added skeleton styling

--- components/pages/home/Feed.jsx
+++ components/pages/home/Feed.jsx
@@ -1,5 +1,5 @@
 "use client";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import {
   fetchAllPostsWithProfiles,
   fetchProfileById,
@@ -7,10 +7,12 @@ import {
   fetchCurrentUser,
 } from "@/constants/db/index";
 import Post from "@/components/shared/cards/Post";
+import SkeletonUi from "../profile/skeletonUi";
 
 export default function Feed() {
   const [posts, setPosts] = useState(null);
   const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const [content, setContent] = useState(null);
   const [error, setError] = useState(null);
 
@@ -24,12 +26,20 @@ export default function Feed() {
       }
     })();
   }, []);
-
-  if (!posts) return <div>Loading...</div>;
+  if (!isLoading)
+    return (
+      <div className="flex flex-wrap justify-center items-center gap-6 mt-6">
+        <SkeletonUi />
+        <SkeletonUi />
+        <SkeletonUi />
+        <SkeletonUi />
+      </div>
+    );
+  // if (!posts) return <div>Loading...</div>;
 
   return (
     <article className="flex flex-col gap-2 w-full max-w-xl">
-      {posts.map((post) => (
+      {posts?.map((post) => (
         <Post key={post.id} post={post} />
       ))}
     </article>
