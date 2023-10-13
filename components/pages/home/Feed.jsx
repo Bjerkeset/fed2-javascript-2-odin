43dd83dddeed49254bb90e43c576b0a71279eadc
Torjnilsen <torjus987@gmail.com>
2023-10-13 14:14:42 +0200

    filter-function-torjus

--- components/pages/home/Feed.jsx
+++ components/pages/home/Feed.jsx
@@ -5,11 +5,13 @@ import Post from "@/components/shared/cards/Post";
 import SkeletonUi from "../profile/skeletonUi";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
+
 export default function Feed({ currentUserId }) {
   const [posts, setPosts] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [currentUser, setCurrentUser] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     (async () => {
@@ -25,6 +27,10 @@ export default function Feed({ currentUserId }) {
       }
     })();
   }, []);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   if (isLoading) {
     return (
@@ -45,9 +51,26 @@ export default function Feed({ currentUserId }) {
     ? posts.filter((post) => post.user_id === currentUserId)
     : posts;
 
+  const searchResults = filteredPosts.filter((post) => {
+    const contentMatch = (post.content || "")
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const authorMatch = (post.author_name || "")
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    return contentMatch || authorMatch;
+  });
+
   return (
-    <article className="flex flex-col gap-2 w-full ">
-      {filteredPosts.map((post) => (
+    <article className="flex flex-col gap-2 w-full p-3 ">
+      <input
+        type="text"
+        placeholder="Search posts or authors..."
+        onChange={handleSearch}
+        value={searchTerm}
+        className="w-full p-2 py-2 px-4 rounded-full border border-gray-300 shadow-md focus:ring-2 focus:ring-blue-400 focus:border-blue-400 placeholder-gray-400"
+      />
+      {searchResults.map((post) => (
         <Dialog key={post.id}>
           <DialogTrigger>
             <Post
