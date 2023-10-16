03e3707d8c7b191c2f2622d32b4696d00369fc32
Bjerkeset <bb.bjerk@gmail.com>
2023-10-16 11:12:45 +0200

    updated AuthButtonServer

--- components/shared/buttons/AuthButtonServer.tsx
+++ components/shared/buttons/AuthButtonServer.tsx
@@ -5,18 +5,11 @@ import AuthButtonClient from "./AuthButtonClient";
 
 export const dynamic = "force-dynamic";
 
-// Define a server action to handle cookie operations
-export async function serverAction(context: {cookies: any}): Promise<any> {
-  const {cookies} = context;
+export default async function AuthButtonServer() {
   const supabase = createServerComponentClient({cookies});
   const {
     data: {session},
   } = await supabase.auth.getSession();
-  return session;
-}
 
-// Update your AuthButtonServer component to use the server action
-export default async function AuthButtonServer(context: {cookies: any}) {
-  const session = await serverAction(context);
   return <AuthButtonClient session={session} />;
 }
