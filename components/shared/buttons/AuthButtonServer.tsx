e5cfa77a89029f86553aa863a68c3983f94ecc2f
Bjerkeset <bb.bjerk@gmail.com>
2023-10-16 11:19:26 +0200

    force-dynamic to layout.tsx

--- components/shared/buttons/AuthButtonServer.tsx
+++ components/shared/buttons/AuthButtonServer.tsx
@@ -2,7 +2,6 @@ import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
 import {create} from "domain";
 import {cookies} from "next/headers";
 import AuthButtonClient from "./AuthButtonClient";
-
 export const dynamic = "force-dynamic";
 
 export default async function AuthButtonServer() {
