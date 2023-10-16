5a62fa163c1377c3b27b9441aa172684e02e7859
Bjerkeset <bb.bjerk@gmail.com>
2023-10-16 10:42:00 +0200

    updated authbuttonserver to fix DynamicServerError

--- components/shared/buttons/AuthButtonServer.tsx
+++ components/shared/buttons/AuthButtonServer.tsx
@@ -1,13 +1,19 @@
+// Import your necessary server libraries
 import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
-import {create} from "domain";
-import {cookies} from "next/headers";
 import AuthButtonClient from "./AuthButtonClient";
 
-export default async function AuthButtonServer() {
+// Define a server action to handle cookie operations
+export async function serverAction(context: {cookies: any}): Promise<any> {
+  const {cookies} = context;
   const supabase = createServerComponentClient({cookies});
   const {
     data: {session},
   } = await supabase.auth.getSession();
+  return session;
+}
 
+// Update your AuthButtonServer component to use the server action
+export default async function AuthButtonServer(context: {cookies: any}) {
+  const session = await serverAction(context);
   return <AuthButtonClient session={session} />;
 }
